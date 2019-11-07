7c84080906167f3f0e568e79e48ab92530acab11
Claiton Denardi <37638849+ClaitonDenardi@users.noreply.github.com>
2019-11-07 09:30:06 -0300

    AddForm ++

--- frontend/src/pages/addForm.js
+++ frontend/src/pages/addForm.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './Main.css';
-
-import './Header.css'
+import './addForm.css';
+import './Header.css';
 
 export default function Header () {
   return(
-    <div className="container">
-      <h1>Em construção...</h1> 
-    </div> 
+    <div>
+      
+      <div className="container">
+        <h1>Em construção..asdasda</h1>
+        <form>
+          
+          <input type="text" name="nomeIES" placeholder="Digite o nome da IES"/>
+          <h2> X</h2>
+          <input type="number" name="IGC2014" placeholder="Insira o IGC Discreto de X"/>
+          <h3>Despesas de X</h3>
+          <input type="number" name="emp" placeholder="Empenhado"/>
+          <input type="number" name="gastp" placeholder="Gasto"/>
+          <input type="number" name="pago" placeholder="Pago"/>
+        </form>
+      </div>
+    </div>
   );
 }
